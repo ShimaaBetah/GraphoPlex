c450cd75135a2c1985ea0cfcd4193c9b38c6e47d
Shimaa Betah <shimaabetah1911@gmail.com>
2023-03-12 01:38:13 +0200

    feat : add handling logic for recieve message from PUT_VERTEX exchange

--- graph_db/src/main/java/com/server/graph_db/rabbitmq/consumer/PutVertexConsumer.java
+++ graph_db/src/main/java/com/server/graph_db/rabbitmq/consumer/PutVertexConsumer.java
@@ -1,28 +1,30 @@
 package com.server.graph_db.rabbitmq.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.stereotype.Service;
 import com.server.graph_db.vertex.Vertex;
+import com.server.graph_db.vertex.VertexService;
 
 @Service
 public class PutVertexConsumer {
 
   
-
+    @Autowired
+    VertexService vertexService;
 
      //listen to specific binding
-     
     @RabbitListener(bindings = @QueueBinding(
         value = @Queue(value = "#{myQueryNameResolver.resolve()}"),
         exchange = @Exchange(value = "PUT_VERTEX", durable = "true", type = "topic"),
         key = "#{myServerProperties.serverId}"
     ))
     public void receiveMessage(Vertex vertex) {
-        System.out.println("Received Message: " + vertex.toString());
+        System.out.println("Received Message") ;
+        vertexService.addVertex(vertex);
     }
     
 }
