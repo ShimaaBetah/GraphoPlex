b6b315712624016b613394419c2fb8879233793d
Shimaa Betah <shimaabetah1911@gmail.com>
2023-04-20 06:24:00 +0200

    feat : add grpc service and client for database config

---
+++ graph_db/src/main/java/com/server/graph_db/grpc/clients/DatabaseConfigClient.java
@@ -0,0 +1,128 @@
+package com.server.graph_db.grpc.clients;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.server.graph_db.exceptions.database.DatabaseAlreadyExist;
+import com.server.graph_db.exceptions.database.DatabaseNotFound;
+import com.server.graph_db.grpc.database.DatabaseName;
+import com.server.graph_db.grpc.database.databaseConfigServiceGrpc;
+import com.server.graph_db.grpc.database.emptyResponse;
+import com.server.graph_db.grpc.database.reply;
+
+import io.grpc.ManagedChannel;
+
+
+@Service
+public class DatabaseConfigClient {
+    @Autowired
+    ManagedChannel channel;
+
+    @Autowired
+    @Qualifier("grpcChannels")
+    Map<String, ManagedChannel> grpcChannels;
+
+
+    public void createDatabase(String database, String serverId) throws Exception {
+        databaseConfigServiceGrpc.databaseConfigServiceBlockingStub blockingStub = databaseConfigServiceGrpc
+                .newBlockingStub(grpcChannels.get(serverId));
+        DatabaseName request = DatabaseName.newBuilder().setName(database).build();
+        reply response;
+        try{
+        response = blockingStub.createDatabase(request);
+        }catch(Exception e){
+            throw new Exception("Internal server error");
+        }
+
+        if(response.getStatus().getCode()==6){
+            throw new DatabaseAlreadyExist(database);
+        }
+    }
+
+    public void deleteDatabase(String database, String serverId) throws Exception {
+        databaseConfigServiceGrpc.databaseConfigServiceBlockingStub blockingStub = databaseConfigServiceGrpc
+                .newBlockingStub(grpcChannels.get(serverId));
+        DatabaseName request = DatabaseName.newBuilder().setName(database).build();
+        reply response;
+        try{
+        response = blockingStub.deleteDatabase(request);
+        }catch(Exception e){
+            throw new Exception("Internal server error");
+        }
+
+        if(response.getStatus().getCode()==5){
+            throw new DatabaseNotFound(database);
+        }
+    }
+
+    public void dropDatabase(String database, String serverId) throws Exception {
+        databaseConfigServiceGrpc.databaseConfigServiceBlockingStub blockingStub = databaseConfigServiceGrpc
+                .newBlockingStub(grpcChannels.get(serverId));
+        DatabaseName request = DatabaseName.newBuilder().setName(database).build();
+        reply response;
+        try{
+        response = blockingStub.dropDatabase(request);
+        }catch(Exception e){
+            throw new Exception("Internal server error");
+        }
+
+        if(response.getStatus().getCode()==5){
+            throw new DatabaseNotFound(database);
+        }
+    }
+
+    public void switchDatabase(String database, String serverId) throws Exception {
+        databaseConfigServiceGrpc.databaseConfigServiceBlockingStub blockingStub = databaseConfigServiceGrpc
+                .newBlockingStub(grpcChannels.get(serverId));
+        DatabaseName request = DatabaseName.newBuilder().setName(database).build();
+        reply response;
+        try{
+        response = blockingStub.switchDatabase(request);
+        }catch(Exception e){
+            throw new Exception("Internal server error");
+        }
+
+        if(response.getStatus().getCode()==5){
+            throw new DatabaseNotFound(database);
+        }
+    }
+
+    public String getCurrentDatabase(String serverId) throws Exception {
+        databaseConfigServiceGrpc.databaseConfigServiceBlockingStub blockingStub = databaseConfigServiceGrpc
+                .newBlockingStub(grpcChannels.get(serverId));
+        emptyResponse request = emptyResponse.newBuilder().build();
+        DatabaseName response;
+        try{
+        response = blockingStub.getCurrentDatabase(request);
+        }catch(Exception e){
+            throw new Exception("Internal server error");
+        }
+
+        return response.getName();
+    }
+
+    public void switchToDefaultDatabase(String serverId) throws Exception {
+        databaseConfigServiceGrpc.databaseConfigServiceBlockingStub blockingStub = databaseConfigServiceGrpc
+                .newBlockingStub(grpcChannels.get(serverId));
+        emptyResponse request = emptyResponse.newBuilder().build();
+        try{
+           blockingStub.switchToDefaultDatabase(request);
+        }catch(Exception e){
+            throw new Exception("Internal server error");
+        }
+    }
+
+    public void dropDefaultDatabase(String serverId) throws Exception {
+        databaseConfigServiceGrpc.databaseConfigServiceBlockingStub blockingStub = databaseConfigServiceGrpc
+                .newBlockingStub(grpcChannels.get(serverId));
+        emptyResponse request = emptyResponse.newBuilder().build();
+        try{
+           blockingStub.dropDefaultDatabase(request);
+        }catch(Exception e){
+            throw new Exception("Internal server error");
+        }
+    }
+}
---
+++ graph_db/src/main/java/com/server/graph_db/grpc/services/DatabaseConfigServiceImpl.java
@@ -0,0 +1,159 @@
+package com.server.graph_db.grpc.services;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.rpc.Status;
+import com.server.graph_db.database.LocalDatabaseService;
+import com.server.graph_db.exceptions.database.DatabaseAlreadyExist;
+import com.server.graph_db.exceptions.database.DatabaseNotFound;
+import com.server.graph_db.grpc.database.DatabaseName;
+import com.server.graph_db.grpc.database.databaseConfigServiceGrpc;
+import com.server.graph_db.grpc.database.emptyResponse;
+import com.server.graph_db.grpc.database.reply;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class DatabaseConfigServiceImpl extends databaseConfigServiceGrpc.databaseConfigServiceImplBase {
+
+    @Autowired
+    LocalDatabaseService databaseService;
+
+
+    @Override
+    public void createDatabase(DatabaseName request,
+            io.grpc.stub.StreamObserver<com.server.graph_db.grpc.database.reply> responseObserver) {
+        try {
+            databaseService.createDatabase(request.getName());
+            //build ok status
+            Status status = Status.newBuilder().setCode(0).build();
+            reply successreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(successreply);
+            responseObserver.onCompleted();
+
+
+        } catch (DatabaseAlreadyExist e) {
+            //build already exist status
+            Status status = Status.newBuilder().setCode(6).build();
+            reply errorreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(errorreply);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void deleteDatabase(DatabaseName request,
+            io.grpc.stub.StreamObserver<com.server.graph_db.grpc.database.reply> responseObserver) {
+        try {
+            databaseService.deleteDatabase(request.getName());
+            //build ok status
+            Status status = Status.newBuilder().setCode(0).build();
+            reply successreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(successreply);
+            responseObserver.onCompleted();
+        }catch(DatabaseNotFound e) {
+            //build not found status
+            Status status = Status.newBuilder().setCode(5).build();
+            reply errorreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(errorreply);
+            responseObserver.onCompleted();
+        }catch(Exception e) {
+            //throw error
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void dropDatabase(DatabaseName request,
+            io.grpc.stub.StreamObserver<com.server.graph_db.grpc.database.reply> responseObserver) {
+        try {
+            databaseService.dropDatabase(request.getName());
+            //build ok status
+            Status status = Status.newBuilder().setCode(0).build();
+            reply successreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(successreply);
+            responseObserver.onCompleted();
+        }catch(DatabaseNotFound e) {
+            //build not found status
+            Status status = Status.newBuilder().setCode(5).build();
+            reply errorreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(errorreply);
+            responseObserver.onCompleted();
+        }catch(Exception e) {
+            //throw error
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void switchDatabase (DatabaseName request,
+            io.grpc.stub.StreamObserver<com.server.graph_db.grpc.database.reply> responseObserver) {
+        try {
+            databaseService.switchDatabase(request.getName());
+            //build ok status
+            Status status = Status.newBuilder().setCode(0).build();
+            reply successreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(successreply);
+            responseObserver.onCompleted();
+        }catch(DatabaseNotFound e) {
+            //build not found status
+            Status status = Status.newBuilder().setCode(5).build();
+            reply errorreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(errorreply);
+            responseObserver.onCompleted();
+        }catch(Exception e) {
+            //throw error
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void switchToDefaultDatabase(emptyResponse request,
+            io.grpc.stub.StreamObserver<com.server.graph_db.grpc.database.reply> responseObserver) {
+        try {
+            databaseService.switchToDefaultDatabase();
+            //build ok status
+            Status status = Status.newBuilder().setCode(0).build();
+            reply successreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(successreply);
+            responseObserver.onCompleted();
+        }catch(Exception e) {
+            //throw error
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void dropDefaultDatabase (emptyResponse request,
+            io.grpc.stub.StreamObserver<com.server.graph_db.grpc.database.reply> responseObserver) {
+        try {
+            databaseService.dropDefaultDatabase();
+            //build ok status
+            Status status = Status.newBuilder().setCode(0).build();
+            reply successreply = reply.newBuilder().setStatus(status).build();
+            responseObserver.onNext(successreply);
+            responseObserver.onCompleted();
+        }catch(Exception e) {
+            //throw error
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void getCurrentDatabase(emptyResponse request,
+            io.grpc.stub.StreamObserver<com.server.graph_db.grpc.database.DatabaseName> responseObserver) {
+        try {
+            String currentDatabase = databaseService.getCurrentDatabase();
+            //build ok status
+            DatabaseName reply = DatabaseName.newBuilder().setName(currentDatabase).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }catch(Exception e) {
+            //build internal error status
+            responseObserver.onError(e);
+            
+        }
+    }
+    
+}
