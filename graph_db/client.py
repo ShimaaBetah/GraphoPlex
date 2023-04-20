633f8b8ce13ec8d799bdc66882fc20493cdddf1d
Shimaa Betah <shimaabetah1911@gmail.com>
2023-04-20 06:26:41 +0200

    feat ; add simple python client that connect to the server to send the query and display the results

---
+++ graph_db/client.py
@@ -0,0 +1,40 @@
+import cmd
+import requests
+import json
+
+
+
+
+class MyShell(cmd.Cmd):
+    intro = 'Welcome to distributed graph db. Type help or ? to list commands.\n'
+    prompt = 'distributed_graphdb >'
+
+    def do_greet(self, arg):
+        'Greet the user.'
+        print('Hello,', arg)
+    
+    def do_ping(self, arg):
+        'Ping the server.'
+        
+        
+    
+    def default(self, line):
+        url = 'http://www.localhost:8080/query'
+        # set header for content type
+        headers = {'Content-Type': 'application/json'}
+        
+        # send post request with body query : line
+        json_data = {"query": line}
+        body = json.dumps(json_data)
+        # print response
+        response = requests.post(url, body, headers = headers)
+        print(response.text)
+
+       
+
+    def do_quit(self, arg):
+        'Exit the shell.'
+        return True
+
+if __name__ == '__main__':
+    MyShell().cmdloop()
