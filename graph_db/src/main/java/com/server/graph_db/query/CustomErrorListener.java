c1bbc0e859365686bfc71bd9b681e2cb0902c7c0
Shimaa Betah <shimaabetah1911@gmail.com>
2023-07-07 23:22:29 +0200

    feat : add custom error listner to return detailed error message for syntax error

---
+++ graph_db/src/main/java/com/server/graph_db/query/CustomErrorListener.java
@@ -0,0 +1,16 @@
+package com.server.graph_db.query;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+public class CustomErrorListener extends BaseErrorListener {
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer,
+                            Object offendingSymbol,
+                            int line, int charPositionInLine,
+                            String msg,
+                            RecognitionException e) {
+        throw new RuntimeException("Syntax error at line " + line + ":" + charPositionInLine + " " + msg);
+    }
+}
